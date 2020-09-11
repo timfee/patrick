b2d50b22cdfed85342dc8a48f795ec8e69435dd2
Tim Feeley <tim@timfeeley.com>
2020-09-11 12:59:40 -0700

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./pages/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+};
